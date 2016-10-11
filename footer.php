ff2da79c49b20c30676313974e15ac7d62f1d930
Clayton Anderson <canderson@Claytons-iMac.local>
2016-10-10 19:27:58 -0700

    Lots of changes..

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php
+    echo '            <div class=contactinfo>
+    <h3 class ="specialname">Contact</h3>
+    <table border="0" style="width:100%">
+    <col width="150px">
+    <tr>
+<td>Email:</td>
+    <td><a href="mailto:ckanders@ucsd.edu">ckanders@ucsd.edu</a></td>
+    </tr>
+    <tr>
+<td>Office:</td>
+    <td>Not Fixed Yet</td>
+    </tr>
+    <tr>
+    <td valign="top">Mailing Address:</td>
+    <td>Clayton Anderson<br>
+    University of California, San Diego<br>
+    Rest TBD<br>
+    </td>
+    </tr>
+    </table>
+    </div>
+    <hr>
+    <div class=copyright>
+    <p>&copy; 2015 <a href="./">Clayton Anderson</a>. All rights reserved.</p>
+    </div>';
+?>
